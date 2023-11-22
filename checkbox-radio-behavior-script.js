94d58b9329ba4fbb913609d66420c6f0c3518660
furylicouzzz <151087421+furylicouzzz@users.noreply.github.com>
2023-11-22 20:25:41 +0300

    Update checkbox-radio-behavior-script.js

--- checkbox-radio-behavior-script.js
+++ checkbox-radio-behavior-script.js
@@ -1,3 +1,7 @@
+// Инициализация выбора опций для групп parametrs-content-items.
+// Активирует checkbox-field-hidden и применяет стили для checkbox-button-field-2 с совпадающими значениями.
+// Также активирует radio-button-field-hidden и применяет стили для radio-button-field-2 с совпадающими значениями.
+// Поведение chekbox и radio button
 $(document).ready(function () {
     // Выбрать все группы parametrs-content-items
     $('.parametrs-content-items').each(function () {
