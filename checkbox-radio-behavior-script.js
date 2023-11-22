e800461c4f2cbe79c761b93be1f754990af934ce
furylicouzzz <151087421+furylicouzzz@users.noreply.github.com>
2023-11-22 20:19:39 +0300

    Create checkbox-radio-behavior-script.js

---
+++ checkbox-radio-behavior-script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // Выбрать все группы parametrs-content-items
+    $('.parametrs-content-items').each(function () {
+      var currentGroup = $(this);
+
+      // Сделать checkbox-field-hidden активным
+      currentGroup.find('.checkbox-field-hidden input[type="checkbox"]').prop('checked', true);
+
+      var checkboxHiddenOption = currentGroup.find('.checkbox-field-hidden .options-name-item').text().trim();
+      currentGroup.find('.checkbox-button-field-2 .options-name-item').each(function () {
+        var checkboxCurrentOption = $(this).text().trim();
+        if (checkboxCurrentOption === checkboxHiddenOption) {
+          // Найдено одинаковое значение, сделать checkbox-button-field-2 активным и применить стили
+          $(this).closest('.checkbox-button-field-2').find('input[type="checkbox"]').prop('checked', true);
+          $(this).closest('.checkbox-button-field-2').find('.w-checkbox-input').addClass('w--redirected-checked');
+        }
+      });
+
+      // Сделать radio-button-field-hidden активным
+      currentGroup.find('.radio-button-field-hidden input[type="radio"]').prop('checked', true);
+
+      var radioHiddenOption = currentGroup.find('.radio-button-field-hidden .options-name-item').text().trim();
+      currentGroup.find('.radio-button-field-2 .options-name-item').each(function () {
+        var radioCurrentOption = $(this).text().trim();
+        if (radioCurrentOption === radioHiddenOption) {
+          // Найдено одинаковое значение, сделать radio-button-field-2 активным и применить стили
+          $(this).closest('.radio-button-field-2').find('input[type="radio"]').prop('checked', true);
+          $(this).closest('.radio-button-field-2').find('.w-form-formradioinput').addClass('w--redirected-checked');
+        }
+      });
+    });
+  });
